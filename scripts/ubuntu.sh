f0c8049b0c2f3064e804e607d297a43a7fda74aa
Justin Hinckley <justin.h.hinckley@gmail.com>
2021-07-24 20:02:03 -0400

    added github cli (gh) installation to scripts

--- scripts/ubuntu.sh
+++ scripts/ubuntu.sh
@@ -11,6 +11,12 @@ wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add
 sudo add-apt-repository -y "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 sudo apt install -y code
 
+echo "adding GitHub CLI key and installing gh with apt =========="
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update
+sudo apt install gh
+
 echo "apt autoremoving programs =========="
 sudo apt autoremove -y
 
